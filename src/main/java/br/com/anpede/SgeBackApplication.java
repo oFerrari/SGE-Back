d76ba60a72eed63666d470feb0c9ae43929f1493
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-08-24 20:49:16 -0400

    Subindo Projeto SGE-BACKEND

---
+++ src/main/java/br/com/anpede/SgeBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.anpede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anpede/SgeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anpede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
