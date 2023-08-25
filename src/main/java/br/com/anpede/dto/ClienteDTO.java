63e1598b4c248445a7a78243761ad11b4603d3d5
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-08-25 19:50:03 -0400

    Subindo Entity & DTO

---
+++ src/main/java/br/com/anpede/dto/ClienteDTO.java
@@ -0,0 +1,99 @@
+package br.com.anpede.dto;
+
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private String tipo;
+	private String CPF;
+	private String endereco;
+	private String telefone;
+	private String email;
+
+	public ClienteDTO() {
+		
+	}
+
+	public ClienteDTO(Long id, String nome, String tipo, String CPF, String endereco, String telefone, String email) {
+
+		this.id = id;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.CPF = CPF;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	
+	public ClienteDTO(ClienteDTO entity) {
+
+		this.id = entity.getId();
+		this.nome = entity.getNome();
+		this.tipo = entity.getTipo();
+		this.CPF = entity.getCPF();
+		this.endereco = entity.getEndereco();
+		this.telefone = entity.getTelefone();
+		this.email = entity.getEmail();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
---
+++ src/main/java/br/com/anpede/entities/Cliente.java
@@ -0,0 +1,117 @@
+package br.com.anpede.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String tipo;
+	private String CPF;
+	private String endereco;
+	private String telefone;
+	private String email;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Long id, String nome, String tipo, String CPF, String endereco, String telefone, String email) {
+
+		this.id = id;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.CPF = CPF;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(CPF, email, endereco, id, nome, telefone, tipo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(CPF, other.CPF) && Objects.equals(email, other.email)
+				&& Objects.equals(endereco, other.endereco) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(telefone, other.telefone)
+				&& Objects.equals(tipo, other.tipo);
+	}
+	
+	
+}
