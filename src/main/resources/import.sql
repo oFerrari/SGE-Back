436c1542702c2cd5410546b780a970e49fb60b64
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-09-15 20:51:11 -0400

    Att

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
 INSERT INTO tb_cliente (id, nome, tipo, documento, endereco, telefone, email) VALUES(1, 'João Silva', 'Cliente', '123.456.789-00', 'Rua A, 123', '(11) 1234-5678', 'joao@example.com');
 INSERT INTO tb_cliente (id, nome, tipo, documento, endereco, telefone, email) VALUES(2, 'Maria Souza', 'Cliente', '987.654.321-00', 'Avenida B, 456', '(22) 9876-5432', 'maria@example.com');
 INSERT INTO tb_cliente (id, nome, tipo, documento, endereco, telefone, email) VALUES(3, 'Carlos Santos', 'Fornecedor', '456.789.123-00', 'Praça C, 789', '(33) 5555-1234', 'carlos@example.com');
+
+
+INSERT INTO tb_motorista (id, nome, CNH, CPF, endereco, telefone, email) VALUES (4, 'João da Silva', '12345678901', '123.456.789-01', 'Rua A 123', '(11) 1234-5678', 'joao.silva@example.com');
+INSERT INTO tb_motorista (id, nome, CNH, CPF, endereco, telefone, email) VALUES (5, 'Maria Souza', '98765432100', '987.654.321-00', 'Avenida B 456', '(22) 9876-5432', 'maria.souza@example.com');
+INSERT INTO tb_motorista (id, nome, CNH, CPF, endereco, telefone, email) VALUES (6, 'Pedro Oliveira', '54321098765', '543.210.987-65', 'Rua C 789', '(33) 2345-6789', 'pedro.oliveira@example.com');
